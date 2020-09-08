b4300b34894f44fcfcdc9dc55d5d4032f10d0ac8
Alexey <57368971+baysnoize@users.noreply.github.com>
2020-09-08 18:25:13 +0300

    commit

---
+++ src/caller-tokenD.js
@@ -0,0 +1,17 @@
+import { ApiCaller } from "@tokend/js-sdk";
+import { WalletsManager } from "@tokend/js-sdk";
+
+export default class TokenD {
+  WalletManager = async () => {
+    const apiCaller = await ApiCaller.getInstanceWithPassphrase(
+      "http://localhost:8060/"
+    );
+    const WManager = new WManager(apiCaller);
+    return WManager;
+  };
+}
+
+CreateWallet = async (email, password) => {
+  const apiCaller = await this.WManager();
+  
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+// import { TokenD } from '@tokend/js-sdk' // import Token
+
+// let sdk = await TokenD.create('https://backend.tokend.com') // создание переменной для импорта
+
 
 Vue.config.productionTip = false;
 
